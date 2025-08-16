97f8d12e6d3b11b5ff4d639c8e329d78b2e9d3ba
yoru <yoru@its.moe>
2025-08-16 07:14:29 -0500

    wip: dashboard

--- blueprints/dashboard.py
+++ blueprints/dashboard.py
@@ -8,6 +8,7 @@ from objects.user import User
 dashboard = Blueprint("dashboard", __name__)
 
 
+@dashboard.route("")
 @dashboard.route("/")
 async def home_get():
     if not User.authenticated():
@@ -17,3 +18,47 @@ async def home_get():
         )
 
     return await render_template("dashboard/home.html")
+
+
+@dashboard.route("/account")
+async def account_get():
+    if not User.authenticated():
+        return await render_template(
+            "login.html",
+            toast=("error", "You are not logged in."),
+        )
+
+    return await render_template("dashboard/account.html")
+
+
+@dashboard.route("/bio")
+async def bio_get():
+    if not User.authenticated():
+        return await render_template(
+            "login.html",
+            toast=("error", "You are not logged in."),
+        )
+
+    return await render_template("dashboard/bio.html")
+
+
+@dashboard.route("/domains")
+async def domains_get():
+    if not User.authenticated():
+        return await render_template(
+            "login.html",
+            toast=("error", "You are not logged in."),
+        )
+
+    return await render_template("dashboard/domains.html")
+
+
+@dashboard.route("/settings")
+async def settings_get():
+    if not User.authenticated():
+        return await render_template(
+            "login.html",
+            toast=("error", "You are not logged in."),
+        )
+
+    return await render_template("dashboard/settings.html")
